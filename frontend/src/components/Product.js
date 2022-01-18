f51e25999133ce21554f3d21d545e1e9baa9c95d
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-01-18 20:21:42 +0530

    react router and product screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+// Component
 import Rating from './Rating';
 
 const Product = (props) => {
     return <Card className='my-3 p-3 rounded'>
-        <a href={`/product/${props.product._id}`}>
+        <Link to={`/product/${props.product._id}`}>
             <Card.Img src={props.product.image} variant='top' />
-        </a>
+        </Link>
 
         <Card.Body>
-            <a href={`/product/${props.product._id}`}>
+            <Link to={`/product/${props.product._id}`}>
                 <Card.Title as='div'>
                     <strong>{props.product.name}</strong>
                 </Card.Title>
-            </a>
+            </Link>
 
             <Card.Text as='div'>
                 <Rating value={props.product.rating} text={`${props.product.numReviews} reviews`} />

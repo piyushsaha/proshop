42131440d4e041509041dda98c97fc5672bf5c8e
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-01-22 22:12:52 +0530

    fetch products from DB; separate /api/products route

--- backend/server.js
+++ backend/server.js
@@ -7,19 +7,16 @@ import products from './data/products.js';
 // DB
 import connectDB from './config/db.js'
 
+// Routes
+import productRoutes from './routes/productRoutes.js';
+
 dotenv.config();
 connectDB();
 
 const app = express();
 
+app.use('/api/products', productRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(product => product._id === req.params.id);
-    res.json(product);
-});

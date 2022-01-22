7800a15e8cc157efc5acb52b9c165292ab639433
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-01-22 23:27:42 +0530

    custom error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from'express';
 import dotenv from 'dotenv';
 
-// Dummy data
-import products from './data/products.js';
-
 // DB
 import connectDB from './config/db.js'
 
 // Routes
 import productRoutes from './routes/productRoutes.js';
+
+// Middlewares
+import { notFound, errorHandler } from './middleware/errorHandler.js';
 
 dotenv.config();
 connectDB();
@@ -16,6 +16,8 @@ connectDB();
 const app = express();
 
 app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));

1acda50d803c1a769a634261878cc02de0de79e7
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-01-23 19:02:42 +0530

    add product constants, action creator and reducer

---
+++ frontend/src/redux/actions/productActions.js
@@ -0,0 +1,24 @@
+import * as productConstants from '../constants/productConstants';
+import axios from 'axios';
+
+// Action creator function
+export const listProducts = () => {
+    return async (dispatch) => {
+        try {
+            // Starts loading
+            dispatch({ type: productConstants.PRODUCT_LIST_REQUEST });
+            
+            const res = await axios.get('/api/products');
+            
+            // Loading successful
+            dispatch({ type: productConstants.PRODUCT_LIST_SUCCESS, payload: res.data });
+        }
+        catch(error) {
+            // Loading unsuccessful
+            dispatch({
+                type: productConstants.PRODUCT_LIST_FAIL,
+                payload: error.response && error.response.data.message ? error.response.data.message : error.message
+            });
+        }
+    }
+}
---
+++ frontend/src/redux/reducers/productReducers.js
@@ -0,0 +1,14 @@
+import * as productConstants from '../constants/productConstants';
+
+export const productListReducer = (state = {products: []}, action) => {
+    switch(action.type) {
+        case productConstants.PRODUCT_LIST_REQUEST:
+            return { loading: true, products: [] };
+        case productConstants.PRODUCT_LIST_SUCCESS :
+            return { loading: false, products: action.payload };
+        case productConstants.PRODUCT_LIST_FAIL :
+            return { loading: false, error: action.payload };
+        default:
+            return state; 
+    }
+}

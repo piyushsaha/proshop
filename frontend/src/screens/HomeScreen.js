f70ce2fed74bf25719e27b96e5d975fc7d9bb4cb
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-01-23 20:50:24 +0530

    using redux store in homescreen to show components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 
 // Components
 import Product from '../components/Product';
 
+// Redux actions
+import { listProducts } from '../redux/actions/productActions';
+
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
+    const productList = useSelector((state) => state.productList);
+    const { products, loading, error } = productList;
     
     useEffect(() => {
-        const fetchProducts = async () => {
-            const res = await axios.get('/api/products');
-            setProducts(res.data);
-        }
-        fetchProducts();
-    }, []);
+        dispatch(listProducts());
+    }, [dispatch]);
     
     return <>
         <h1>Latest Products</h1>
-        <Row>
+        {/* While loading */}
+        {loading && <h2>Loading...</h2>}
+        
+        {/* Error or display fetch products */}
+        {error ? <h2>{error}</h2> : ( <Row>
             {/* Rendering the products in columns */}
             {products.map((product) => {
                 return <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product} />
                 </Col>
             })}
-        </Row>
+        </Row> )}
+        
     </>
 }
 

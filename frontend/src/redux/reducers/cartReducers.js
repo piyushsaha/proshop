74c00ae240e88c5bc52aeba4038262b78a123184
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-01-26 23:57:40 +0530

    fix cart reducer

--- frontend/src/redux/reducers/cartReducers.js
+++ frontend/src/redux/reducers/cartReducers.js
@@ -4,12 +4,12 @@ export const cartReducer = (state={ cartItems: []}, action) => {
     switch(action.type) {
         case cartConstants.CART_ADD_ITEM:
             const item = action.payload;
-            const exists = state.items.find((p) => p.product === item.product);
+            const exists = state.cartItems.find((p) => p.product === item.product);
             if(exists) {
                 return {...state, cartItems: state.cartItems.map(x => x.product === exists.product ? item : x) };
             }
             else {
-                return {...state, cartItems: [...state.cartItem, item]};
+                return {...state, cartItems: [...state.cartItems, item]};
             }
         default:
             return state;

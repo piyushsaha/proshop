84f03266348d553ab114ecb4826900279618c644
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-01-26 23:59:11 +0530

    add to cart functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
-const CartScreen = () => {
+// Redux actions
+import { addToCart } from '../redux/actions/cartActions';
+
+const CartScreen = (props) => {
+    const productId = props.match.params.id;
+    const qty = props.location.search ? Number(props.location.search.split('=')[1]) : 1;
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if(productId) {
+            dispatch(addToCart(productId, qty));
+        }
+    }, [dispatch, productId, qty]);
+    
+    
     return <h4>Cart</h4>
 }
 

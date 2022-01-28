b166e0768f14c011b1554665e65059a2b182fb4d
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-01-28 19:10:21 +0530

    hashing password before saving to DB

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,9 +28,18 @@ const userSchema = mongoose.Schema({
 
 // Normal function keyword is used instead of arrow function
 // This is because the this keyword inside an arrow function is the global scope object which is not correct for our use case
-userSchema.methods.matchPassword = async function(enteredPassword) {
+userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 }
+
+// Hashing password before saving
+userSchema.pre('save', async function () {
+    // Only runs if the password is being modified
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+});
 
 const User = mongoose.model('User', userSchema);
 

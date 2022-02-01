e6c13b49afb7f0184fb321bb46b72ab9b95991f1
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-02-02 00:11:58 +0530

    checkout steps breadcrumb

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 // Components
 import FormContainer from '../components/UI/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 // Redux actions
 import { saveShippingAddress } from '../redux/actions/cartActions';
@@ -26,6 +27,7 @@ const ShippingScreen = (props) => {
     }
     
     return <FormContainer>
+        <CheckoutSteps step1 step2 />
         <h1>Shipping</h1>
         <Form onSubmit={submitHandler}>
         <Form.Group controlId='address'>

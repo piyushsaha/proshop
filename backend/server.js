bde493b8cfd9128232f570bbcf3c2e8a97a59e24
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-02-08 19:39:44 +0530

    PayPal client ID endpoint

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,11 @@ app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 
+// Endpoint to get the PayPal client ID
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-

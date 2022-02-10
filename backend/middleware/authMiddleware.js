171f924f1c46e2d3330876a248f6506e9893f08e
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-02-10 19:04:18 +0530

    admin privilege check middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -35,4 +35,14 @@ const protect = asyncHandler(async (req, res, next) => {
     }
 })
 
-export { protect };
+const admin = (req, res, next) => {
+    if(req.user.isAdmin) {
+        next();
+    }
+    else {
+        res.status(401);
+        throw new Error('Not authorized as admin');
+    }
+}
+
+export { protect, admin };

7a3efa1e8865130e9dd6bb9674e0472c5485be77
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-04-10 22:36:58 +0530

    listen for requests on process.env.PORT

--- backend/server.js
+++ backend/server.js
@@ -52,4 +52,4 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
